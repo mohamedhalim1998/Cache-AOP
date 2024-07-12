be27a01e7fcd1b428ceef8c118970cbfc1fda951
Mohamed Halim <mohamedhalim1998@gmail.com>
2024-07-12 17:33:42 +0300

    initial commit

---
+++ src/main/java/com/mohamed/halim/cacheaop/CacheAopApplication.java
@@ -0,0 +1,13 @@
+package com.mohamed.halim.cacheaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheAopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mohamed/halim/cacheaop/CacheAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohamed.halim.cacheaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
