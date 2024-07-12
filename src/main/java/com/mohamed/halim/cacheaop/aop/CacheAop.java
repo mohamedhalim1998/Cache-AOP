1d2611447ac4de16edd93e3a761dc834d0735944
Mohamed Halim <mohamedhalim1998@gmail.com>
2024-07-12 22:25:52 +0300

    ADD: improve AOP to include function params

--- src/main/java/com/mohamed/halim/cacheaop/aop/CacheAop.java
+++ src/main/java/com/mohamed/halim/cacheaop/aop/CacheAop.java
@@ -3,6 +3,7 @@ package com.mohamed.halim.cacheaop.aop;
 import com.mohamed.halim.cacheaop.service.CacheManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.HashMap;
 
 @Aspect
 @Component
@@ -22,8 +24,9 @@ public class CacheAop {
     @Around("@annotation(Cached)")
     public Object cacheResult(ProceedingJoinPoint joinPoint) throws Throwable {
         Method method = ((MethodSignature) joinPoint.getSignature()).getMethod();
+
         var cacheAnnotation = method.getAnnotation(Cached.class);
-        var key = cacheAnnotation.key();
+        var key = constructKey(cacheAnnotation, method, joinPoint.getArgs());
         if (cacheManager.exist(key)) {
             log.info("from cache: {}", key);
             return cacheManager.get(key);
@@ -33,5 +36,27 @@ public class CacheAop {
             log.info("save to cache: {}", key);
             return res;
         }
+    }
+
+    private String constructKey(Cached cacheAnnotation, Method method, Object[] args) {
+        StringBuilder builder = new StringBuilder();
+        if (StringUtils.isBlank(cacheAnnotation.prefix())) {
+            builder.append(method.getName());
+        } else {
+            builder.append(cacheAnnotation.prefix());
+        }
+
+        if (cacheAnnotation.key() == null || cacheAnnotation.key().length == 0) {
+            var key = Arrays.stream(args).map(Object::toString).reduce("", (s1, s2) -> s1 + "::" + s2);
+            builder.append(key);
+        } else {
+            var paramMap = new HashMap<String, String>();
+            for (int i = 0; i < args.length; i++) {
+                paramMap.put(method.getParameters()[i].getName(), args[i].toString());
+            }
+            var key = Arrays.stream(cacheAnnotation.key()).map(paramMap::get).reduce("", (s1, s2) -> s1 + "::" + s2);
+            builder.append(key);
+        }
+        return builder.toString();
     }
 }
--- src/main/java/com/mohamed/halim/cacheaop/aop/Cached.java
+++ src/main/java/com/mohamed/halim/cacheaop/aop/Cached.java
@@ -8,5 +8,6 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Cached {
-    String key() default "";
+    String prefix() default "";
+    String[] key();
 }
